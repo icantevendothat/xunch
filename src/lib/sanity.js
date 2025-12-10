0f1f637c8af1acd3688b686140b8c55780935a6b
Annika Santhanam <aps659@nyu.edu>
2025-12-10 14:10:34 -0500

    react backend

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -1,17 +1,16 @@
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, 
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,      
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID, 
+  dataset: process.env.REACT_APP_SANITY_DATASET,      
   apiVersion: '2025-12-10',                             
   useCdn: true,                                           
 });
 
-
 export const previewClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  dataset: process.env.REACT_APP_SANITY_DATASET,
+  token: process.env.REACT_APP_SANITY_API_READ_TOKEN,            
   apiVersion: '2025-12-10',
-  token: process.env.SANITY_API_READ_TOKEN,            
   useCdn: false,                                        
 });
